cec117106d9a73c55919aa21c33b9ff4b6294e17
Juan Canneva <juancanneva@setandforget.io>
2024-03-25 09:23:38 -0300

    added button to download extension

--- src/components/login-module/Login.jsx
+++ src/components/login-module/Login.jsx
@@ -1,7 +1,6 @@
 import GoogleBtn from "./GoogleBtn";
 
 const Login = () => {
-
   return (
     <div className="flex min-h-full flex-1">
       <div className="flex flex-col flex-1 px-4 p-y-12 my-24 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
@@ -11,7 +10,13 @@ const Login = () => {
             <h1 className="font-bold">&nbsp;Formula Studio&nbsp;</h1>
             <p className="font-black bg-green-gradient bg-clip-text text-transparent">{`}`}</p>
           </div>
-          <div className="mt-10 max-w-sm pt-[40px] pl-[40px] pr-[40px] pb-[16px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full flex justify-center">
+
+          <a className="flex w-fit items-center justify-center gap-3 rounded-full mt-[40px] px-6 py-3 text-neutral-800 dark:text-neutral-50 dark:bg-gray-800 border border-solid focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2" href="https://chromewebstore.google.com/detail/formula-studio/kippcmdklcbfedimbifbdkkemfcejcna?hl">
+            Download Chrome Extension
+          </a>
+          </div>
+          <div className="mt-10 max-w-sm pt-[40px] pl-[40px] pr-[40px] pb-[40px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div className="max-w-[250px] m-auto">
               <h2 className="mb-2 text-xl text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                 Log in
@@ -19,14 +24,12 @@ const Login = () => {
               <div className="mt-6 flex w-full justify-center">
                 <GoogleBtn />
               </div>
-              
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-
 };
 
 export default Login;
